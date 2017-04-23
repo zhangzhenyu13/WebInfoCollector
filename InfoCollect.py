2a3db18209b12dc62d38f5b2bc3c216cedba9bca
zhangzhenyu13 <zhangzhenyu13@outlook.com>
2017-04-23 21:29:10 +0800

    Update InfoCollect.py

--- InfoCollect.py
+++ InfoCollect.py
@@ -7,14 +7,6 @@ r="resut.csv"
 mainPage="mainDistrict.csv"
 if os.path.exists(path)==False:
     os.mkdir(path)
-
-choice=(input("1/2?\n"))
-if choice in [1,2]:
-    print "after finished ,move *.csv to  dir 'saved'"
-if(choice==1):
-    print "crawl for Pages Urls"
-    cmdline.execute("scrapy crawl pageinfo".split())
-elif choice==2:
-    print "crawl for houseInfo"
-    cmdline.execute("scrapy crawl houseinfo".split())
+print "crawl for houseInfo"
+cmdline.execute("scrapy crawl houseinfo".split())
 
